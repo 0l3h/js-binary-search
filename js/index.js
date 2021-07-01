1621645f7abb6ae50d8aaa12efbc19ad14a64c8a
0l3h <kubik7950@gmail.com>
2021-07-01 08:50:15 +0300

    removed array of numbers

--- js/index.js
+++ js/index.js
@@ -6,22 +6,17 @@
  * @returns Error code or correct number. 
  */
 function guessNumber() {
-    let array = [];
-    for (let i = 0; i < 100; i++) {
-        array.push(i + 1);
-    }
-
     let start = 0;
-    let end = array.length - 1;
+    let end = 100;
     let middle;
 
     do {
         middle = Math.ceil((start + end) / 2);
 
-        if (confirm(`Is your number equals ${array[middle]}?`)) {
-            return array[middle];
+        if (confirm(`Is your number equals ${middle}?`)) {
+            return middle;
         }
-        if (confirm(`Is your number less than ${array[middle]}?`)) {
+        if (confirm(`Is your number less than ${middle}?`)) {
             end = middle - 1;
         } else {
             start = middle + 1;
